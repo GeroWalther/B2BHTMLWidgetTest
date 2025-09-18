ec06ba66ee7bd9a3923cf8ae57dda3fcba89cd60
GeroWalther <gero.walther@gmail.com>
2025-09-18 10:21:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Widget from './components/Widget';
+
+function App() {
+  return (
+    <>
+      <h1>B2B AI チャットウイジェット HTML Test</h1>
+      {/* <Widget /> */}
+    </>
+  );
+}
+
+export default App;
